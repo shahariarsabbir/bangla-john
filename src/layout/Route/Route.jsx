3509f2c0ad7d1e70ea1dc332b59860530fe1dc9b
shahariarsabbir <217337202+shahariarsabbir@users.noreply.github.com>
2026-07-13 17:55:54 +0600

    fix netlify router basename

--- src/layout/Route/Route.jsx
+++ src/layout/Route/Route.jsx
@@ -7,49 +7,44 @@ import Manageinventory from '../../components/manageInventory/Manageinventory'
 import OrderReview from '../../components/orderReview/OrderReview'
 import NotFound from '../../shared/NotFound/NotFound'
 
-const Router = createBrowserRouter(
-	[
-		{
-			path: '/',
-			element: <Main />,
-			children: [
-				{
-					index: true,
-					element: <Home />,
-				},
-				{
-					path: 'shop',
-					element: <Shop />,
-				},
-				{
-					path: 'manage-inventory',
-					element: <Manageinventory />,
-				},
-				{
-					path: 'order-review',
-					element: <OrderReview />,
-					loader: async () => {
-						const response = await fetch(
-							`${import.meta.env.BASE_URL}product.json`,
-						)
-
-						if (!response.ok) {
-							throw new Error('Failed to load products')
-						}
-
-						return response.json()
-					},
-				},
-				{
-					path: '*',
-					element: <NotFound />,
-				},
-			],
-		},
-	],
+const Router = createBrowserRouter([
 	{
-		basename: '/bangla-john',
+		path: '/',
+		element: <Main />,
+		children: [
+			{
+				index: true,
+				element: <Home />,
+			},
+			{
+				path: 'shop',
+				element: <Shop />,
+			},
+			{
+				path: 'manage-inventory',
+				element: <Manageinventory />,
+			},
+			{
+				path: 'order-review',
+				element: <OrderReview />,
+				loader: async () => {
+					const response = await fetch(
+						`${import.meta.env.BASE_URL}product.json`,
+					)
+
+					if (!response.ok) {
+						throw new Error('Failed to load products')
+					}
+
+					return response.json()
+				},
+			},
+			{
+				path: '*',
+				element: <NotFound />,
+			},
+		],
 	},
-)
+])
 
 export default Router
